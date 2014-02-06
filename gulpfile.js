0632be663b960d14bdc77c88cbbbeb8f5b41bef9
Adrian Cooney <cooney.adrian@gmail.com>
2014-02-06 06:14:25 +0000

    Gulp minification task

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require("gulp"),
+	rename = require("gulp-rename"),
 	uglify = require("gulp-uglify");
 
 gulp.task("minify", function() {
 	gulp.src("src/tinyrequire.js")
 		.pipe(uglify())
-		.pipe(gulp.dest("src/tinyrequire.min.js"));
+		.pipe(rename("tinyrequire.min.js"))
+		.pipe(gulp.dest("src/"));
 });
