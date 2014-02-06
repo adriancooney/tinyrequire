6116f3b828c82850bee869670f76aaf97b1071c2
Adrian Cooney <cooney.adrian@gmail.com>
2014-02-06 06:55:15 +0000

    Moving to static filename

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require("gulp"),
 	rename = require("gulp-rename"),
-	uglify = require("gulp-uglify");
+	uglify = require("gulp-uglify"),
+	gzip = require("gulp-gzip");
 
 gulp.task("minify", function() {
 	gulp.src("src/tinyrequire.js")
 		.pipe(uglify())
+		.pipe(gzip())
 		.pipe(rename("tinyrequire.min.js"))
 		.pipe(gulp.dest("src/"));
 });
